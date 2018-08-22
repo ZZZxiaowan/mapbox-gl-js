b40efd8c317dda03af175dac62550ba4629caaea
ZZZxiaowan <42559256+ZZZxiaowan@users.noreply.github.com>
2018-08-22 11:26:53 +0800

    Translate down 100%
    line 2 to line 5

--- docs/pages/example/add-image.js
+++ docs/pages/example/add-image.js
@@ -1,6 +1,8 @@
 /*---
 title: Add an icon to the map
+title:给地图添加图标
 description: Add an icon to the map from an external URL and use it in a symbol layer.
+description:给地图添加外部URL的图标并能在符号图层使用。
 tags:
   - styles
   - layers
